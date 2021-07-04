56ff0b0079527bc140ed75aa5eb6a0dbe565adf8
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-07-05 02:44:06 +0800

    MaskThrow Code

--- MaskGame/Assets/Scripts/MaskThrow.cs
+++ MaskGame/Assets/Scripts/MaskThrow.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MaskThrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        ShootMask();
     }
+
+    void ShootMask()
+	{
+        if(Input.touchCount > 0)
+		{
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touch.phase == TouchPhase.Began)
+			{
+                
+			}
+		}
+	}
 }
