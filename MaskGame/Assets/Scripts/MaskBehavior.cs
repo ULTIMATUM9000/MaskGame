b13fe1a826e34b9ac7b868f32268f52bc4ee8c0b
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-07-05 18:44:06 +0800

    Small Update

--- MaskGame/Assets/Scripts/MaskBehavior.cs
+++ MaskGame/Assets/Scripts/MaskBehavior.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MaskBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject MaskProjectile;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DragMask();
+    }
+
+    void DragMask()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Screen Touched");
+            }
+            if(touch.phase == TouchPhase.Moved)
+			{
+
+			}
+        }
     }
 }
