7e37e456e57a0d249548c60549a1af4d8e8493bc
ULTIMATUM9000 <kylepogideleon@gmail.com>
2021-07-25 00:03:15 +0800

    small changes

--- MaskGame/Assets/Scripts/MainMenu.cs
+++ MaskGame/Assets/Scripts/MainMenu.cs
@@ -11,10 +11,10 @@ public class MainMenu : MonoBehaviour
 	}
 	public void LoadScene(string sceneName)
     {
-		CanvasManager.instance.SwitchCanvas(CanvasType.GameUI);
 		GameManager.instance.InitializeGame();
         AudioManager.instance.Stop("MainMenuBGM");
         AudioManager.instance.Play("GameBGM");
         SceneManager.LoadScene(sceneName);
-    }
+		CanvasManager.instance.SwitchCanvas(CanvasType.GameUI);
+	}
 }
